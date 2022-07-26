02b8df7b8dccfefce15876ec474a93a741ea4594
Gagandatt <gagandattrewala@gmail.com>
2022-07-26 18:30:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  let [data, setData] = useState([])
+  useEffect(() => {
+    let fetchData = async () => {
+      const URL = "https://api.github.com/users/gagandatt"
+      let response = await fetch(URL)
+      let finalData = await response.json()
+       console.log(finalData)
+      setData(finalData)
+    }
+    fetchData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container text-center my-5">
+        <div className="row">
+          <div className="col">
+            <div className="card" style={{width: "18rem"}}>
+              <img src={data.avatar_url} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{data.name}</h5>
+                  <p className="card-text">{data.bio}</p>
+                  <a href={data.html_url} target="_blank" className="btn btn-primary">Github</a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
